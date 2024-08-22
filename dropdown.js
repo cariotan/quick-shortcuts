66b242d749cf740bffa1a260ee785826351b49c8
Cario Tan <a@cario.me>
2024-08-22 15:10:31 +1000

    Initial commit

---
+++ dropdown.js
@@ -0,0 +1,21 @@
+// Make a div containing the input and ul dropdown. set ul display to none. the id passed to the function is the id of the div container.
+function initSelect(id) {
+	let $propertyTypeSelect = $('#' + id);
+	let $select = $propertyTypeSelect.find('input');
+	let $ul = $propertyTypeSelect.find('ul');
+
+	$select.on('focus', function () {
+		$ul.show();
+	});
+
+	$ul.children().on('click', function () {
+		$select.val($(this).text());
+		$ul.hide();
+	});
+
+	$(document).on('click', function(event) {
+		if(!$(event.target).closest('#' + id).length) {
+			$ul.hide();
+		}
+	});
+}
---
+++ modal.js
@@ -0,0 +1,35 @@
+// The modal content div should have class hidden.
+function initModal(id) {
+	let $errorModal = $('#' + id);
+
+	let $modal = $('<div class="fixed inset-0 flex justify-center items-center" style="display: none; z-index: 10000">');
+	let $backdrop = $('<div class="fixed inset-0 bg-black/80" style="opacity: 0"></div>');
+
+	$errorModal.wrap($modal);
+	$modal = $errorModal.css({ opacity: 0 }).removeClass('hidden').parent();
+	$modal.prepend($backdrop);
+
+	$backdrop.on('click', closeModal);
+
+	function openModal() {
+		$errorModal.css({ scale: 0.5 });
+		$modal.show();
+		$backdrop.transition({ opacity: 1 });
+		$errorModal.transition({ opacity: 1, scale: 1 });
+	}
+
+	function closeModal() {
+		$backdrop.transition({ opacity: 0 });
+		$errorModal.transition({ opacity: 0, scale: 0.5 });
+		setTimeout(function () {
+			$modal.hide()
+		}, 400)
+	}
+
+	let modal = {
+		openModal: openModal,
+		closeModal: closeModal,
+	};
+
+	return modal;
+} 
