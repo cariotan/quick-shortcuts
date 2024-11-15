3526968a8f1c60d8c5545ec9a2728131ab0ab46f
cariotan <a@cario.me>
2024-11-15 13:06:16 +1100

    Update IdentityContext.cs

--- IdentityContext.cs
+++ IdentityContext.cs
@@ -1,12 +1,12 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-	public class IdentityContext : IdentityDbContext
-	{
-		private static string DbPath = "C:\\Database\\IdentityContext.db";
+public class IdentityContext : IdentityDbContext
+{
+	private static string DbPath = "C:\\Database\\IdentityContext.db";
 
-		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-		{
-			optionsBuilder.UseSqlite($"Data Source={DbPath};Pooling=false");
-		}
+	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+	{
+		optionsBuilder.UseSqlite($"Data Source={DbPath};Pooling=false");
 	}
+}
